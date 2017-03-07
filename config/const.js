aac45aaf7be5ea5397860a8cbd206ddab2368daf
NicolasBouhours <nico53960@hotmail.fr>
2017-03-07 20:43:47 +0100

    Basic express starter

---
+++ config/const.js
@@ -0,0 +1,7 @@
+const WEB_PORT = process.env.PORT || 8000
+const ENV = process.env.NODE_ENV || 'development'
+
+module.exports = {
+  WEB_PORT,
+  ENV
+}
--- server.js
+++ server.js
@@ -1,1 +1,39 @@
-console.log('Hello Kat')
+const compression = require('compression')
+const express = require('express')
+const morgan = require('morgan')
+const mongoose = require('mongoose')
+const bodyParser = require('body-parser')
+const helmet = require('helmet')
+const cors = require('cors')
+const join = require('join')
+
+const { WEB_PORT, ENV } = require('./config/const')
+
+const app = express()
+
+app.use(compression())
+
+// Static file middleware
+app.use('/static', express.static(__dirname + '/public'))
+
+// Desactivate log if test environment
+if (ENV !== 'test') app.use(morgan())
+
+// parse body params and attache them to req.body
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+
+// secure apps by setting various HTTP headers
+app.use(helmet())
+
+// enable CORS - Cross Origin Resource Sharing
+app.use(cors())
+
+app.get('/', (req, res) => {
+  res.send('hello')
+})
+
+app.listen(WEB_PORT, () => {
+  // eslint-disable-next-line no-console
+  console.log(`Server running on port ${WEB_PORT} ${ENV}.`)
+})
